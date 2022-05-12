dbb51a1632156a16b4233c027cbde77997000b9a
Brunoaqu <bruno.gl.aquino@gmail.com>
2022-05-12 11:47:43 -0300

    Created first route: saveFile

--- src/modules/File.Module.ts
+++ src/modules/File.Module.ts
@@ -1,12 +1,15 @@
 import { Schema, model } from 'mongoose';
 import { IFile } from './IFile';
 
-const FileSchema = new Schema<IFile>({
-  name: { type: String },
-  path: { type: String },
-  size: { type: Number },
-  user: { type: String },
-});
+const FileSchema = new Schema<IFile>(
+  {
+    name: { type: String },
+    path: { type: String },
+    size: { type: Number },
+    user: { type: String },
+  },
+  { versionKey: false },
+);
 
 const File = model<IFile>('User', FileSchema);
 
