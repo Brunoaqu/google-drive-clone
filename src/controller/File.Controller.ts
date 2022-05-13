1a2308c6fc17e37debced6e29918b2a18e4e24a2
Brunoaqu <bruno.gl.aquino@gmail.com>
2022-05-12 21:09:22 -0300

    Add function to manage used storage

--- src/controller/File.Controller.ts
+++ src/controller/File.Controller.ts
@@ -5,23 +5,40 @@ import { IFileSchema } from '../modules/IFileSchema';
 import { File } from '../modules/File.Module';
 
 export class FileController {
+  // Function to save file in system and his informations on MONGO database
   public async saveFile(request: express.Request, response: express.Response):Promise<void> {
     try {
-      const upfile = request.file;
       const _username = request.body._username || os.hostname();
-      const file = new File({
-        name: upfile?.filename,
-        path: `${process.cwd()}/uploads/${upfile?.filename}`,
-        filesize: upfile?.size * 0.00097656,
-        user: _username,
-      });
-      const result = await file.save();
-      response.json(result);
-    } catch (error) {
-      response.json({ error });
+      const upfile = request.file;
+      const files: Array<IFileSchema> = await File.find({ user: _username });
+      const _path = `${process.cwd()}/uploads/${upfile?.filename}`;
+
+      let total = 0;
+      for (let i = 0; i < files.length; i += 1) {
+        total += files[i].filesize;
+      }
+
+      if (total + upfile.size <= 300) {
+        const file = new File({
+          name: upfile?.filename,
+          path: _path,
+          filesize: upfile?.size * 0.00097656,
+          user: _username,
+        });
+        const result = await file.save();
+        response.status(200).json(result);
+      } else {
+        fs.unlink(_path, (error) => {
+          if (error) throw error;
+        });
+        throw new Error('Você atingiu seu limite de armazenamento');
+      }
+    } catch (error: any) {
+      response.status(400).json({ error: error.message });
     }
   }
 
+  // Function to get file
   public async getFile(request: express.Request, response: express.Response):Promise<void> {
     try {
       const result: Array<IFileSchema> = await File.find({ name: request.params._filename });
@@ -32,6 +49,7 @@ export class FileController {
     }
   }
 
+  // Function to count how many files user have uploaded
   public async fileCount(request: express.Request, response: express.Response):Promise<void> {
     try {
       const _username = request.query._username || os.hostname();
@@ -42,15 +60,18 @@ export class FileController {
     }
   }
 
+  // Function to calculate how much space user have used
   public async getUsedStorage(request: express.Request, response: express.Response):Promise<void> {
     try {
       const _username = request.query._username || os.hostname();
+
       const result: Array<IFileSchema> = await File.find({ user: _username });
 
       let total = 0;
       for (let i = 0; i < result.length; i += 1) {
         total += result[i].filesize;
       }
+
       response.json({ armazenamento: total });
     } catch (error) {
       response.json(error);
